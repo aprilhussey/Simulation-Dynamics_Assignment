7a68785a999820843d82ae1dc9691bdd1e657958
April Hussey <april.hussey@outlook.com>
2024-03-27 15:20:37 +0000

    Starting to implement buttons for tools, pots, soil, and seeds

--- Green Thumb/Assets/Scripts/Scriptable Objects/ItemData.cs
+++ Green Thumb/Assets/Scripts/Scriptable Objects/ItemData.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class ItemData : ScriptableObject
 {
 	// Shared characteristics between all items
 	public string itemName;
-	
+
+	public Sprite itemSprite;
+
 	[TextArea(3,20)]
 	public string itemDescription;
 }
