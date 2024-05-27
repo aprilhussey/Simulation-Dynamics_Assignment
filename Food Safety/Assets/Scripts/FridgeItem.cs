03d7b07b8d4839a4f5a392f75c37e744823e7f73
April Hussey <april.hussey@outlook.com>
2024-05-27 08:17:25 +0100

    Added fridge items to FridgeLayout scene and added to MainMenu scene

---
+++ Food Safety/Assets/Scripts/FridgeItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeItem : MonoBehaviour
+{
+	public string itemName;
+
+	public bool isReadyToEat;
+	public bool isDairyProduct;
+    public bool isMeat;
+	public bool isFruitOrVeg;
+}
